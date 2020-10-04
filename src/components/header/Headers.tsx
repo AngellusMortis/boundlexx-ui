78cbb40e0e878e68f3bb4e8b2ad08170ac21628e
Samantha Taylor <staylor@mort.is>
2020-10-03 20:50:36 -0400

    WIP on header

---
+++ src/components/header/Headers.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Headers extends React.Component {
+    render() {
+        return (
+            <div><h1>Header Here</h1></div>
+        )
+    }
+}
+
+export default Headers;
